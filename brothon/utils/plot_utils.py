26cc41d75ffa3363943801e8d432476ea8c6ca13
Brian Wylie <briford.wylie@gmail.com>
2017-08-22 18:55:51 -0600

    putting import try around matplotlib

--- brothon/utils/plot_utils.py
+++ brothon/utils/plot_utils.py
@@ -1,18 +1,21 @@
 """Plotting utilities"""
 def plot_defaults():
-    import matplotlib.pyplot as plt
-    plt.style.use('seaborn-muted')
-    plt.rc('font', size=14)
-    plt.rc('xtick', labelsize=12)
-    plt.rc('ytick', labelsize=12)
-    plt.rc('axes', labelsize=14)
-    plt.rc('axes', axisbelow=True)
-    plt.rc('grid', color='grey')
-    plt.rc('grid', alpha=.5)
-    plt.rc('patch', linewidth=1)
-    plt.rc('patch', force_edgecolor=True)
-    plt.rcParams['figure.figsize'] = 10.0, 4.0
-    plt.rcParams.update({'figure.autolayout': True})
+    try:
+        import matplotlib.pyplot as plt
+        plt.style.use('seaborn-muted')
+        plt.rc('font', size=14)
+        plt.rc('xtick', labelsize=12)
+        plt.rc('ytick', labelsize=12)
+        plt.rc('axes', labelsize=14)
+        plt.rc('axes', axisbelow=True)
+        plt.rc('grid', color='grey')
+        plt.rc('grid', alpha=.5)
+        plt.rc('patch', linewidth=1)
+        plt.rc('patch', force_edgecolor=True)
+        plt.rcParams['figure.figsize'] = 10.0, 4.0
+        plt.rcParams.update({'figure.autolayout': True})
+    except ImportError:
+        print('Could not import matplotlib... this is fine... nothing bad...')
 
 def test():
     """Test the Plot Utilities"""
