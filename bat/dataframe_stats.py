c5e33dac7aaa527462d4aa92856e58fde0ef4850
Brian Wylie <briford.wylie@gmail.com>
2017-11-01 12:49:17 -0600

    add an additional check to avoid div by 0

--- bat/dataframe_stats.py
+++ bat/dataframe_stats.py
@@ -88,7 +88,7 @@ def compute_g(count, expected):
     """G Test Score for log likelihood ratio
        - http://en.wikipedia.org/wiki/G_test (Wikipedia)
     """
-    return 2.0 * count * math.log(count/expected) if count else 0
+    return 2.0 * count * math.log(count/expected) if count and expected else 0
 
 
 # Simple test of the functionality
