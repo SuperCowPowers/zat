e93e79363b05a4eae57df6db81a4c95cd1f31f60
Brian Wylie <briford.wylie@gmail.com>
2019-10-28 14:15:20 -0600

    adding a zeek log to parquet example

--- examples/bro_to_parquet.py
+++ examples/bro_to_parquet.py
@@ -3,17 +3,33 @@ from __future__ import print_function
 import os
 import sys
 import argparse
+from pyspark.sql import SparkSession
 
 # Local imports
-from bat.log_to_parquet import log_to_parquet
+from bat import log_to_sparkdf
+
+
+# Helper method
+def log_to_parquet(log_in, parquet_out):
+    # Spin up a local Spark Session (with 4 executors)
+    spark = SparkSession.builder.master('local[4]').appName('my_awesome').getOrCreate()
+
+    # Use the BAT class to load our log file into a Spark dataframe (2 lines of code!)
+    spark_it = log_to_sparkdf.LogToSparkDF(spark)
+    spark_df = spark_it.create_dataframe(log_in)
+
+    # Write it out as a parquet file
+    spark_df.write.parquet(parquet_out)
+    print('{:s} --> {:s}'.format(log_in, parquet_out))
+
 
 if __name__ == '__main__':
     # Example to write Parquet file from a bro log
 
     # Collect args from the command line
     parser = argparse.ArgumentParser()
-    parser.add_argument('bro_log', type=str, help='Specify a bro log to run BroLogReader test on')
-    parser.add_argument('-o', '--parquet-file', type=str, required=True, help='Specify the parquet file to write to')
+    parser.add_argument('zeek_log', type=str, help='Specify the zeek log input file')
+    parser.add_argument('parquet_file', type=str, help='Specify the parquet file to write out')
     args, commands = parser.parse_known_args()
 
     # Check for unknown args
@@ -22,10 +38,9 @@ if __name__ == '__main__':
         sys.exit(1)
 
     # File may have a tilde in it
-    if args.bro_log and args.parquet_file:
-        args.bro_log = os.path.expanduser(args.bro_log)
+    if args.zeek_log and args.parquet_file:
+        args.zeek_log = os.path.expanduser(args.zeek_log)
         args.parquet_file = os.path.expanduser(args.parquet_file)
 
         # Write out the parquet file
-        print('Writing Parquet file: {:s}'.format(args.parquet_file))
-        log_to_parquet(args.bro_log, args.parquet_file)
+        log_to_parquet(args.zeek_log, args.parquet_file)
