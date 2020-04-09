53fd6a8951e3d0d62f2557cae67a70bad5defce4
Branden Wagner <branden.wagner@sentinel.kitware.com>
2020-04-09 15:51:27 -0400

    Fixing typos. A little overzealous on the find and replace of Bro to Zeek

--- zat/zeek_log_reader.py
+++ zat/zeek_log_reader.py
@@ -62,6 +62,21 @@ class ZeekLogReader(file_tailer.FileTailer):
         # Initialize the Parent Class
         super(ZeekLogReader, self).__init__(self._filepath, full_read=True, tail=self._tail)
 
+
+    def file_type(filename):
+        magic_dict = {
+            "\x1f\x8b\x08": "gz",
+            "\x42\x5a\x68": "bz2",
+            "\x50\x4b\x03\x04": "zip"
+        }
+        max_len = max(len(x) for x in magic_dict)
+        with open(filename) as f:
+            file_start = f.read(max_len)
+            for magic, filetype in magic_dict.items():
+                if file_start.startswith(magic):
+                    return filetype
+            return "no match"
+
     def readrows(self):
         """The readrows method reads in the header of the Zeek log and
             then uses the parent class to yield each row of the log file
