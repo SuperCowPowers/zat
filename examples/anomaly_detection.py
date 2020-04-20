cd3bcf7319c49babee84ac84358412f87733f703
Brian Wylie <briford.wylie@gmail.com>
2020-04-20 07:25:07 -0600

    getting rid of SettingWithCopyWarning

--- examples/anomaly_detection.py
+++ examples/anomaly_detection.py
@@ -81,7 +81,7 @@ if __name__ == '__main__':
         # Now we create a new dataframe using the prediction from our classifier
         predictions = odd_clf.predict(bro_matrix)
         odd_df = bro_df[features][predictions == -1]
-        display_df = bro_df[predictions == -1]
+        display_df = bro_df[predictions == -1].copy()
 
         # Now we're going to explore our odd observations with help from KMeans
         odd_matrix = to_matrix.fit_transform(odd_df)
