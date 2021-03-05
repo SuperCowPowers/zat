d8b30f4f0398aabcac8dda6a6228d2dc61866f70
Brian Wylie <briford.wylie@gmail.com>
2021-03-05 10:51:02 -0700

    improve/fix the parquet example

--- examples/zeek_to_parquet.py
+++ examples/zeek_to_parquet.py
@@ -3,8 +3,36 @@ import os
 import sys
 import argparse
 
+# Note: We're going to import pyarrow but it currently has an open issue around supporting time deltas
+#    - https://issues.apache.org/jira/browse/ARROW-6780 so we have to convert timedelta fields to str
+# Also see:
+#   - https://stackoverflow.com/questions/53893554/transfer-and-write-parquet-with-python-and-pandas-got-timestamp-error
+#
+from datetime import timedelta
+import pandas as pd
+try:
+    import pyarrow
+except ImportError:
+    print('Please > pip install pyarrow')
+    sys.exit(1)
+
 # Local imports
 from zat.log_to_dataframe import LogToDataFrame
+
+
+# Helper method for temporarily converting timedelta to string
+def convert_timedelta_to_str(df):
+    delta_columns = df.select_dtypes(include=['timedelta'])
+    for column in delta_columns:
+        df[column] = df[column].apply(tdelta_value_to_str)
+    return df
+
+
+def tdelta_value_to_str(value):
+    if pd.isnull(value):
+        return '-'  # Standard for Zeek null value
+    else:
+        return str(timedelta(seconds=value.total_seconds()))
 
 
 if __name__ == '__main__':
@@ -29,5 +57,10 @@ if __name__ == '__main__':
         # Convert to dataframe and write out the parquet file
         log_to_df = LogToDataFrame()
         zeek_df = log_to_df.create_dataframe(args.zeek_log)
-        zeek_df.to_parquet(args.parquet_file, compression='gzip')  # Snappy is better, but often not installed
+        print('Dataframe Created: {:d} rows...'.format(len(zeek_df)))
+
+        # Check for any timedelta fields (see note above)
+        df = convert_timedelta_to_str(zeek_df)
+
+        zeek_df.to_parquet(args.parquet_file, compression='snappy', use_deprecated_int96_timestamps=True)
         print('Complete: {:s} --> {:s}'.format(args.zeek_log, args.parquet_file))
